088fe98a8c3768d684e9bbf7ed83b941bd9c7b83
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2023-02-13 19:02:26 -0500

    movieReducer done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
+import { deleteMovie } from '../actions/movieActions'
 
 const Movie = (props) => {
     const { id } = useParams();
@@ -8,6 +9,11 @@ const Movie = (props) => {
 
     const movies = props.movies;
     const movie = props.movies.find(movie=>movie.id===Number(id));
+
+    const handleDeleteClick = () => {
+        props.deleteMovie(movie.id);
+        push('/movies');
+    }
     
     return(<div className="modal-page col">
         <div className="modal-dialog">
@@ -39,7 +45,7 @@ const Movie = (props) => {
                         
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete" onClick={handleDeleteClick}><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
@@ -54,4 +60,4 @@ const mapStateToProps = state => {
     })
 }
 
-export default connect(mapStateToProps, {})(Movie);
+export default connect(mapStateToProps, {deleteMovie})(Movie);
